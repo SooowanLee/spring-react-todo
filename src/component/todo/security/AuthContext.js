3c2a23fa239e104c52d9dba2550bb11df721e227
Soowan Lee <lsy9406@gmail.com>
2024-04-23 16:38:44 +0900

    add LoginComponent의 인증 로직을 login()으로 옮김

--- src/component/todo/security/AuthContext.js
+++ src/component/todo/security/AuthContext.js
@@ -11,8 +11,20 @@ export default function AuthProvider({ children }) {
 
   //   setInterval(() => setNumber(number + 1), 10000);
 
+  function login(username, password) {
+    if (username === "testName" && password === "test") {
+      setAuthenticated(true);
+      return true;
+    } else {
+      setAuthenticated(false);
+      return false;
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ number, isAuthenticated, setAuthenticated }}>
+    <AuthContext.Provider
+      value={{ number, isAuthenticated, setAuthenticated, login }}
+    >
       {children}
     </AuthContext.Provider>
   );

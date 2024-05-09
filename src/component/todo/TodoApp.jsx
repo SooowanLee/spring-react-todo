ac3a63edea5f49a82220636012ea71aa6c16e3b2
Soowan Lee <lsy9406@gmail.com>
2024-05-09 18:27:21 +0900

    add Todo 상세페이지 라우팅 추가

--- src/component/todo/TodoApp.jsx
+++ src/component/todo/TodoApp.jsx
@@ -7,6 +7,8 @@ import ErrorComponent from "./ErrorComponent";
 import WelcomeComponent from "./WelcomeComponent";
 import LoginComponent from "./LoginComponent";
 import AuthProvider, { useAuth } from "./security/AuthContext";
+import TodoComponent from "./TodoComponent";
+
 import "./TodoApp.css";
 
 function AuthenticatedRoute({ children }) {
@@ -40,6 +42,14 @@ export default function TodoApp() {
               element={
                 <AuthenticatedRoute>
                   <ListTodosComponent />
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/todo/:id"
+              element={
+                <AuthenticatedRoute>
+                  <TodoComponent />
                 </AuthenticatedRoute>
               }
             />

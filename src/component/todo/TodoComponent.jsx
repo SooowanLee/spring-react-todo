55025834c4ffc06baf8db16cf4ea7aba55f84061
Soowan Lee <lsy9406@gmail.com>
2024-05-10 17:05:12 +0900

    feat Formik 라이브러리 추가

--- src/component/todo/TodoComponent.jsx
+++ src/component/todo/TodoComponent.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 export default function TodoComponent() {
   const { id } = useParams();
   const [description, setDescription] = useState("");
+  const [targetDate, setTargetDate] = useState("");
   const authContext = useAuth();
   const username = authContext.username;
 
@@ -13,14 +15,49 @@ export default function TodoComponent() {
 
   function retrieveTodos() {
     retrieveTodoApi(username, id)
-      .then((response) => setDescription(response.data.description))
+      .then((response) => {
+        setDescription(response.data.description);
+        setTargetDate(response.data.targetDate);
+      })
       .catch((error) => error);
+  }
+
+  function onSubmit(values) {
+    console.log(values);
   }
 
   return (
     <div className="container">
       <h1>Enter Todo Details</h1>
-      <div>Description : {description}</div>
+      <div>
+        <Formik
+          initialValues={{ description, targetDate }}
+          enableReinitialize={true}
+          onSubmit={onSubmit}
+        >
+          {(props) => (
+            <Form>
+              <fieldset className="form-group">
+                <label>Description</label>
+                <Field
+                  type="text"
+                  className="form-control"
+                  name="description"
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <label>Target Date</label>
+                <Field type="date" className="form-control" name="targetDate" />
+              </fieldset>
+              <div>
+                <button className="btn btn-success m-5" type="submit">
+                  Save
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }

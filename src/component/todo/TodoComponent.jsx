8e8ee9b42b29f2cc7a9f99ab273b2e5ce220f716
Soowan Lee <lsy9406@gmail.com>
2024-05-13 14:26:03 +0900

    feat 유효성 검증 추가

--- src/component/todo/TodoComponent.jsx
+++ src/component/todo/TodoComponent.jsx
@@ -1,29 +1,81 @@
-import { useParams } from "react-router-dom";
-import { retrieveTodoApi } from "./api/TodoApiService";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  createTodoApi,
+  retrieveTodoApi,
+  updateTodoApi,
+} from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import moment from "moment";
 
 export default function TodoComponent() {
   const { id } = useParams();
   const [description, setDescription] = useState("");
   const [targetDate, setTargetDate] = useState("");
   const authContext = useAuth();
+  const navigate = useNavigate();
   const username = authContext.username;
 
   useEffect(() => retrieveTodos(), [id]);
 
   function retrieveTodos() {
-    retrieveTodoApi(username, id)
-      .then((response) => {
-        setDescription(response.data.description);
-        setTargetDate(response.data.targetDate);
-      })
-      .catch((error) => error);
+    if (id != -1) {
+      retrieveTodoApi(username, id)
+        .then((response) => {
+          setDescription(response.data.description);
+          setTargetDate(response.data.targetDate);
+        })
+        .catch((error) => error);
+    }
   }
 
   function onSubmit(values) {
     console.log(values);
+    const todo = {
+      id: id,
+      username: username,
+      description: values.description,
+      targetDate: values.targetDate,
+      done: false,
+    };
+
+    console.log(todo);
+
+    if (id == -1) {
+      createTodoApi(username, todo)
+        .then(navigate("/todos"))
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      updateTodoApi(username, id, todo)
+        .then((response) => {
+          navigate("/todos");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  function validate(values) {
+    let errors = {};
+
+    if (values.description.length < 5) {
+      errors.description = "최소 5자이상 입력하세요.";
+    }
+
+    if (
+      values.targetDate == null ||
+      values.targetDate == "" ||
+      moment(values.targetDate).isValid()
+    ) {
+      errors.targetDate = "유효한 날짜를 입력하세요.";
+    }
+
+    console.log(values);
+    return errors;
   }
 
   return (
@@ -34,9 +86,22 @@ export default function TodoComponent() {
           initialValues={{ description, targetDate }}
           enableReinitialize={true}
           onSubmit={onSubmit}
+          validate={validate}
+          validateOnChange={false}
+          validateOnBlur={false}
         >
           {(props) => (
             <Form>
+              <ErrorMessage
+                name="description"
+                component="div"
+                className="alert alert-warning"
+              />
+              <ErrorMessage
+                name="targetDate"
+                component="div"
+                className="alert alert-warning"
+              />
               <fieldset className="form-group">
                 <label>Description</label>
                 <Field

10689eed05b2a3080fb9d783bb59874c78484438
Soowan Lee <lsy9406@gmail.com>
2024-05-13 14:27:22 +0900

    feat 인증 헤더 추가

--- src/component/todo/api/HelloWorldApiService.js
+++ src/component/todo/api/HelloWorldApiService.js
@@ -5,4 +5,8 @@ const apiClient = axios.create({ baseURL: "http://localhost:8080" });
 export const retrieveHelloWorldBean = () => apiClient.get("/hello-world-bean");
 
 export const retrieveHelloWorldPathVariable = (username) =>
-  apiClient.get(`/hello-world/path-variable/${username}`);
+  apiClient.get(`/hello-world/path-variable/${username}`, {
+    headers: {
+      Authorization: "Basic dGVzdGVyMTp0ZXN0 ",
+    },
+  });

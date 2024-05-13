ff9fbc4a171547d881604bc2a6bb7721d5d2f5dd
Soowan Lee <lsy9406@gmail.com>
2024-05-13 14:26:30 +0900

    feat Todo 생성, 수정 API 추가

--- src/component/todo/api/TodoApiService.js
+++ src/component/todo/api/TodoApiService.js
@@ -10,3 +10,9 @@ export const deleteTodoApi = (username, id) =>
 
 export const retrieveTodoApi = (username, id) =>
   apiClient.get(`/users/${username}/todos/${id}`);
+
+export const updateTodoApi = (username, id, todo) =>
+  apiClient.put(`/users/${username}/todos/${id}`, todo);
+
+export const createTodoApi = (username, todo) =>
+  apiClient.post(`users/${username}/todos`, todo);
